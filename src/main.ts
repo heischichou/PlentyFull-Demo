f4c4c2b569d2d93895e5a56f1e3d84f4085076b8
Heischichou <jangarot@gmail.com>
2023-06-02 21:28:16 +0800

    Fix Boostrap 5 configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
 
 import "./assets/styles/app.scss";
 
@@ -11,5 +11,3 @@ import router from "./router";
 import store from "./store";
 
 createApp(App).use(store).use(router).mount("#app");
-
-import "bootstrap/dist/js/bootstrap.js";
