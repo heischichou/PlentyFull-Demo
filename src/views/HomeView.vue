1622dbcb7bf2c73d7e555924cb57561486fd1cf3
Francine Menchavez <francia.menchavez054@gmail.com>
2023-06-06 20:59:11 +0800

    [feature/104] completed homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <div class="container-fluid bg-header" id="header">
       <div class="container flex-column flex-md-row">
         <div
-          class="row d-flex align-items-center g-5 py-5 order-last order-md-fist"
+          class="row d-flex align-items-center g-5 py-5 order-last order-lg-first"
         >
           <div class="col-lg-6 container text-start">
             <h2 class="text-white fw-bold display-5 mb-5">
@@ -15,7 +15,7 @@
               animal molestiae te. Ex duo eripuit mentitum.
             </p>
             <div class="d-grid gap-3 d-md-flex justify-content-md-start mt-5">
-              <button type="button" class="btn btn-primary rounded-0">
+              <button type="button" class="btn btn-primary rounded-0 p-3">
                 Get Started
               </button>
               <button type="button" class="btn btn-outline-primary rounded-0">
@@ -23,13 +23,150 @@
               </button>
             </div>
           </div>
-          <div class="col-lg-6 py-4 container order-first order-md-last">
+          <div class="col-lg-6 py-4 container order-first order-lg-last">
             <img
               class="img-fluid"
               src="@/assets/images/HomeView/Header_Image.svg"
               alt="Header_Image"
             />
           </div>
+        </div>
+      </div>
+    </div>
+    <!---------------intersted?------------->
+    <div class="container py-5">
+      <div class="row d-flex align-items-center order-last order-md-fist pt-3">
+        <div class="col-lg-6 container text-center pt-3">
+          <h2 class="fw-bold display-5 mb-5">Interested?</h2>
+        </div>
+      </div>
+      <div
+        class="row d-flex align-items-center g-5 py-5 order-last order-md-fist"
+      >
+        <div class="col-lg-6 py-4 container">
+          <img
+            class="img-fluid"
+            src="@/assets/images/HomeView/Interested_image.svg"
+            alt="Interested_image"
+          />
+        </div>
+        <div class="col-lg-6 container text-start px-4">
+          <p>
+            Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam
+            no suscipit quaerendum. At nam minimum ponderum. Est audiam animal
+            molestiae te. Ex duo eripuit mentitum.
+          </p>
+          <h2 class="fw-bold mb-3 mt-5">Join Us!</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            dignissim justo eget velit congue, sed semper velit pulvinar.
+            Integer fermentum tempus urna, id eleifend nibh efficitur vitae.
+            Nunc dapibus mi lectus, a maximus risus vestibulum ac. Nam sem quam,
+            tincidunt feugiat tristique vel, dictum at diam. Phasellus vel ante
+            semper, molestie lacus a, consequat augue. Donec commodo, nisl quis
+            tristique mattis, nibh metus bibendum nunc, non accumsan magna purus
+            ut lectus.
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-------------About Us------------------>
+    <div class="container-fluid o-bg py-5">
+      <div class="row d-flex align-items-center order-last order-md-fist">
+        <div class="col-lg-6 container text-center pt-3">
+          <h3 class="fw-bold display-5 mb-5">About Us</h3>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row d-flex align-items-center order-last order-md-fist">
+          <div class="col-md-4 px-4 py-3 container text-center">
+            <em class="bi bi-boxes fs-1"></em>
+            <h4 class="pt-3">Auto Responder</h4>
+            <p>
+              Assum suavitate ea vel, vero erat doming cu cum. Zril ornatus sea
+              cu. Pro ex integre pertinax. Cu cum harum paulo legendos, mei et
+              quod enim mnesarchum.
+            </p>
+          </div>
+          <div class="col-md-4 px-4 py-3 container text-center">
+            <em class="bi bi-graph-up-arrow fs-1"></em>
+            <h4 class="pt-3">Soicial Plugins</h4>
+            <p>
+              Eos tota dicunt democritum no. Has natum gubergren ne. Est viris
+              soleat sadipscing cu. Legere epicuri insolens eu nec, dicit
+              virtute urbanitas id nam, qui in habeo semper eligendi.
+            </p>
+          </div>
+          <div class="col-md-4 px-4 py-3 container text-center">
+            <em class="bi bi-database fs-1"></em>
+            <h4 class="pt-3">Data Backup</h4>
+            <p>
+              Ne nam phaedrum consequat, adhuc aliquid ea pri, eum eligendi
+              incorrupte referrentur in. Vix ad senserit salutandi argumentum.
+              Ei eam definiebas reformidans, exerci persecuti no ius.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6 py-4 container order-first order-lg-last">
+        <img
+          class="img-fluid"
+          src="@/assets/images/HomeView/About_image.svg"
+          alt="Header_Image"
+        />
+      </div>
+    </div>
+    <!-------------Product Statistics------------>
+    <div class="container py-5">
+      <div class="row d-flex align-items-center order-last order-md-fist py-3">
+        <div class="col-lg-4 container text-start pt-5">
+          <h3 class="fw-bold mb-5">Product Statistics</h3>
+          <p>
+            Assum suavitate ea vel, vero erat doming cu cum. Zril ornatus sea
+            cu. Pro ex integre pertinax. Cu cum harum paulo legendos, mei et
+            quod enim mnesarchum.
+          </p>
+          <p>
+            Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam
+            no suscipit quaerendum. At nam minimum ponderum. Est audiam animal
+            molestiae te. Ex duo eripuit mentitum.
+          </p>
+        </div>
+        <div class="col-lg-6 container text-center pt-3">
+          <img
+            class="img-fluid"
+            src="@/assets/images/HomeView/Statistics_image.svg"
+            alt="Statistics_image"
+          />
+          <div
+            class="d-grid gap-3 d-md-flex justify-content-md-start mt-5 pt-3"
+          >
+            <button type="button" class="btn btn-primary rounded-0 p-3">
+              See More Details
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!----------------Ready?-------------------->
+    <div class="container-fluid o-ready pt-3">
+      <div class="container align-items-center pt-5">
+        <h4>Ready to get started?</h4>
+        <p class="pt-3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+          dignissim justo eget velit congue, sed semper velit pulvinar.
+        </p>
+        <div class="d-grid gap-3 d-md-flex justify-content-md-center pt-3">
+          <button type="button" class="btn btn-primary rounded-0 p-3">
+            Register Now
+          </button>
+        </div>
+        <div class="col-lg-6 py-5 container order-first order-lg-last">
+          <img
+            class="img-fluid mt-3"
+            src="@/assets/images/HomeView/Members_image.svg"
+            alt="Members_Image"
+          />
         </div>
       </div>
     </div>
@@ -43,3 +180,13 @@ export default defineComponent({
   name: "HomeView",
 });
 </script>
+
+<style lang="scss">
+.o-bg {
+  background-color: rgba(#56f576, 0.2);
+}
+
+.o-ready {
+  background-color: rgba(#45bf5d, 0.7);
+}
+</style>
