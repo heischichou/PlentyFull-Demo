11abf33fa37ae934f96a45b70469e63112298dde
Francine Menchavez <francia.menchavez054@gmail.com>
2023-06-06 12:27:17 +0800

    [feature/104] made header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class="container-fluid bg-header" id="header">
+      <div class="container flex-column flex-md-row">
+        <div
+          class="row d-flex align-items-center g-5 py-5 order-last order-md-fist"
+        >
+          <div class="col-lg-6 container text-start">
+            <h2 class="text-white fw-bold display-5 mb-5">
+              If you want to eliminate hunger, everybody has to be involved
+            </h2>
+            <p class="text-white">
+              Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad,
+              nam no suscipit quaerendum. At nam minimum ponderum. Est audiam
+              animal molestiae te. Ex duo eripuit mentitum.
+            </p>
+            <div class="d-grid gap-3 d-md-flex justify-content-md-start mt-5">
+              <button type="button" class="btn btn-primary rounded-0">
+                Get Started
+              </button>
+              <button type="button" class="btn btn-outline-primary rounded-0">
+                Learn More
+              </button>
+            </div>
+          </div>
+          <div class="col-lg-6 py-4 container order-first order-md-last">
+            <img
+              class="img-fluid"
+              src="@/assets/images/HomeView/Header_Image.svg"
+              alt="Header_Image"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
 });
 </script>
