56e715857ae5ad4c4201b653fb1fe01c43599e47
Heischichou <jangarot@gmail.com>
2023-06-21 19:32:32 +0800

    [feature/209] Completed role-based conditional rendering

--- src/views/StatisticsView.vue
+++ src/views/StatisticsView.vue
@@ -1,12 +1,52 @@
 <template>
-  <div class="container-fluid bg-primary">
+  <div
+    class="container-fluid"
+    :class="role === 'Administrator' ? 'bg-header' : 'bg-primary'"
+  >
     <div class="container">
-      <div class="row flex-column flex-lg-row align-items-center py-5">
+      <div
+        class="row flex-column flex-lg-row align-items-center py-5"
+        :class="role === 'Administrator' ? 'text-white' : 'text-black'"
+      >
         <div class="col col-lg-6">
-          <img class="img-fluid" src="@/assets/images/Statistics/Header.svg" />
+          <img
+            v-if="role === 'Administrator'"
+            class="img-fluid"
+            src="@/assets/images/Statistics/Admin_Header.svg"
+          />
+
+          <img
+            v-else
+            class="img-fluid"
+            src="@/assets/images/Statistics/Header.svg"
+          />
         </div>
         <div class="col col-lg-6 mt-5 mt-lg-0">
-          <div class="text-start">
+          <div v-if="role === 'Administrator'" class="text-center">
+            <h1 class="mb-5 fw-bold">Recent Updates</h1>
+            <div class="row">
+              <div class="col-12 col-md">
+                <h2 class="fw-semibold mb-4">{{ donationCount }}</h2>
+                <h6 class="fw-semibold mb-3">Food Donation Count</h6>
+                <small class="opacity-75">
+                  This count shows the number of confirmed transactions made by
+                  Food Donors through out the week.
+                </small>
+              </div>
+              <div class="col-12 col-md mt-5 mt-md-0">
+                <h2 class="fw-semibold mb-4">
+                  {{ successfulTransactionsCount }}
+                </h2>
+                <h6 class="fw-semibold mb-3">Successful Transactions Count</h6>
+                <small class="opacity-75">
+                  This count shows the number of confirmed transactions of
+                  Charity Organizations through out the week.
+                </small>
+              </div>
+            </div>
+          </div>
+
+          <div v-else class="text-start">
             <h2 class="mb-3 fw-semibold">Your Statistics</h2>
             <p class="text-justify">
               Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad,
@@ -143,9 +183,15 @@ export default defineComponent({
   },
   computed: {
     figuresTheme(): string[] {
-      return this.role === "Donor" || this.role === "Charity"
+      return this.role !== "Administrator"
         ? ["#113333", "#45bf5D", "#56f576", "#6ef997"]
         : ["#ffffff", "#45bf5D", "#56f576", "#6ef997"];
+    },
+    donationCount(): number | null {
+      return this.role === "Administrator" ? 0 : null;
+    },
+    successfulTransactionsCount(): number | null {
+      return this.role === "Administrator" ? 0 : null;
     },
   },
   created() {
