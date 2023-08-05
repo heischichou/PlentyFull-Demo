02bb26fa8ad4be1dcbc483b71c6e67920a3dfbe5
Francine Menchavez <francia.menchavez054@gmail.com>
2023-08-05 22:18:59 +0800

    [feature/413_19]Created both tests for Manage User

---
+++ tests/e2e/specs/Admin/manage-users.cy.js
@@ -0,0 +1,76 @@
+describe("Manage Users Page", () => {
+  const baseUrl = "http://localhost:8081/registry";
+  beforeEach(() => {
+    cy.visit(baseUrl);
+  });
+
+  it("should exist", () => {
+    cy.get("#app").should("exist");
+  });
+
+  it("should display page title", () => {
+    cy.get("h1").should("exist");
+    cy.get("h1").should("be.visible");
+  });
+
+  it("should contain and display a sidebar", () => {
+    cy.get("#sideBar").should("exist");
+  });
+
+  it("should contain and display a button and three items", () => {
+    cy.get("#sideBar").find(".navbar-toggler").should("exist");
+    cy.get("#sideBar").find(".navbar-toggler").should("be.visible");
+    cy.get("#sideBar").find(".nav-link").should("have.length", 3);
+    cy.get("#sideBar").find(".nav-link").should("be.visible");
+  });
+
+  it("should switch between Settings tabs correctly", async () => {
+    const tabs = [
+      {
+        tab: "registrationTab",
+        content: "requestsSection",
+      },
+      {
+        tab: "membersTab",
+        content: "manageUsers",
+      },
+      {
+        tab: "reportsTab",
+        content: "userReports",
+      },
+    ];
+
+    tabs.forEach((tab) => {
+      cy.get(`#${tab.tab}`)
+        .should("exist")
+        .click()
+        .then(($el) => {
+          cy.get(`#${tab.content}`).should("exist").should("be.visible");
+        });
+    });
+  });
+
+  it("should contain and display actions buttons", () => {
+    cy.get("#membersTab")
+      .click()
+      .then(($el) => {
+        cy.get("a").find(".bi-exclamation-circle").should("exist");
+        cy.get("a").find(".bi-exclamation-circle").should("be.visible");
+        cy.get("a").find(".bi-lock").should("exist");
+        cy.get("a").find(".bi-lock").should("be.visible");
+        cy.get("a").find(".bi-dash-circle").should("exist");
+        cy.get("a").find(".bi-dash-circle").should("be.visible");
+      });
+  });
+
+  it("should display a table with 6 columns", () => {
+    cy.get("#membersTab")
+      .click()
+      .then(($el) => {
+        cy.get("table").should("exist");
+        cy.get("table").should("be.visible");
+        cy.get("table").find("th").should("have.length", 6);
+        cy.get("table").find("th").should("be.visible");
+      });
+  });
+});
